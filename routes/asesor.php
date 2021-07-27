f153cdb0c5ce1e36f262f315053fbc4c541f8bd7
samcep <samuelcepeda301@gmail.com>
2021-07-27 15:52:33 -0500

    roles y servicios

--- routes/asesor.php
+++ routes/asesor.php
@@ -4,4 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('dash/asesor/usuario', function () {
             return view('dash.usuario.index');
-});
+})->middleware('can:dash.usuario.index');
