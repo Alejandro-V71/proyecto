1bba84cb0ebcc91f0c0d435db00ba93473dfa905
samcep <samuelcepeda301@gmail.com>
2021-07-26 19:21:15 -0500

    auth y dashboard

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,10 @@ Route::get('/', function () {
 
 Auth::routes(['verify' => true]);
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes();
+
+
+Route::get('/dash', [App\Http\Controllers\HomeController::class, 'index'])->name('dash');
+
 
 
