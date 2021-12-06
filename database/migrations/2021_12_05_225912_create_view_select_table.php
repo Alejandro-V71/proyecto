9932378f0e1d0d4cc8ac15959a656ca25dd81f18
Alejandro-V71 <83032425+Alejandro-V71@users.noreply.github.com>
2021-12-06 11:04:09 -0500

    Update 2021_12_05_225912_create_view_select_table.php

--- database/migrations/2021_12_05_225912_create_view_select_table.php
+++ database/migrations/2021_12_05_225912_create_view_select_table.php
@@ -15,61 +15,89 @@ class CreateViewSelectTable extends Migration
     public function up()
     {
         DB::statement("
-        BEGIN;
-        SET TRANSACTION READ WRITE;
         
-        CREATE VIEW reporteEstado AS SELECT DISTINCT
-        lineas."nombreLinea" AS nombreLinea,
-        marcas."nombreMarca" AS nombreMarca,
-        categorias."nombreCategoria" AS nombreCategoria,
-        motocicletas."id" AS id,
-        motocicletas."placaMotocicleta" AS placaMotocicleta,
-        motocicletas."colorMotocicleta" AS colorMotocicleta,
-        motocicletas."cilindraje" AS cilindraje,
-        motocicletas."kilometraje" AS kilometraje,
-        motocicletas."categoria_id" AS categoria_id,
-        motocicletas."marca_id" AS marca_id,
-        users."name" AS NAME,
-        users."email" AS email,
-        solicitud_servicios."id" AS solicitud_id,
-        solicitud_servicios."title" AS title,
-        solicitud_servicios."horaSolcitudServicio" AS horaSolcitudServicio,
-        solicitud_servicios."descripcionProblema" AS descripcionProblema,
-        solicitud_servicios."Start" AS START,
-        solicitud_servicios."End" AS END,
-    estado_solicitud_servicio."estado_id" AS estado_id,
-    detalle_solicituds."diagnostico" AS diagnostico,
-    servicios."nombreServicio" AS nombreServicio,
-    servicios."precioTotal" AS precioTotal,
-    detalle_solicitud_repuesto."detalle_solicitud_id" AS detalle_solicitud_id
+        
+        CREATE VIEW reporteEstado AS
+        
+        SELECT DISTINCT
+    `lineas`.`nombreLinea` AS `nombreLinea`,
+    `marcas`.`nombreMarca` AS `nombreMarca`,
+    `categorias`.`nombreCategoria` AS `nombreCategoria`,
+    `motocicletas`.`id` AS `id`,
+    `motocicletas`.`placaMotocicleta` AS `placaMotocicleta`,
+    `motocicletas`.`colorMotocicleta` AS `colorMotocicleta`,
+    `motocicletas`.`cilindraje` AS `cilindraje`,
+    `motocicletas`.`kilometraje` AS `kilometraje`,
+    `motocicletas`.`categoria_id` AS `categoria_id`,
+    `motocicletas`.`marca_id` AS `marca_id`,
+    `users`.`name` AS `name`,
+    `users`.`email` AS `email`,
+    `solicitud_servicios`.`id` AS `solicitud_id`,
+    `solicitud_servicios`.`title` AS `title`,
+    `solicitud_servicios`.`horaSolcitudServicio` AS `horaSolcitudServicio`,
+    `solicitud_servicios`.`descripcionProblema` AS `descripcionProblema`,
+    `solicitud_servicios`.`Start` AS `start`,
+    `solicitud_servicios`.`End` AS `end`,
+    `estado_solicitud_servicio`.`estado_id` AS `estado_id`,
+    `detalle_solicituds`.`diagnostico` AS `diagnostico`,
+    `servicios`.`nombreServicio` AS `nombreServicio`,
+    `servicios`.`precioTotal` AS `precioTotal`,
+    `detalle_solicitud_repuesto`.`detalle_solicitud_id` AS `detalle_solicitud_id`
 FROM
-    motocicletas
-JOIN lineas ON(
-        lineas."id" = motocicletas."linea_id"
-    )
-JOIN marcas ON(
-        marcas."id" = motocicletas."marca_id"
-    )
-JOIN categorias ON(
-        categorias."id" = motocicletas."categoria_id"
-    )
-JOIN users ON(
-        users."id" = motocicletas."user_id"
-    )
-JOIN solicitud_servicios ON(
-        solicitud_servicios."user_id" = users."id"
-    )
-JOIN estado_solicitud_servicio ON(
-        estado_solicitud_servicio."solicitud_id" = solicitud_servicios."id"
-    )
-JOIN detalle_solicituds ON(
-        detalle_solicituds."solicitud_servicio_id" = solicitud_servicios."id"
-    )
-JOIN servicios ON(
-        servicios."id" = detalle_solicituds."servicio_id"
-    )
-JOIN detalle_solicitud_repuesto ON(
-        detalle_solicitud_repuesto."detalle_solicitud_id" = detalle_solicituds."id"
+    (
+        (
+            (
+                (
+                    (
+                        (
+                            (
+                                (
+                                    (
+                                        `motocicletas`
+                                    JOIN `lineas` ON
+                                        (
+                                            `lineas`.`id` = `motocicletas`.`linea_id`
+                                        )
+                                    )
+                                JOIN `marcas` ON
+                                    (
+                                        `marcas`.`id` = `motocicletas`.`marca_id`
+                                    )
+                                )
+                            JOIN `categorias` ON
+                                (
+                                    `categorias`.`id` = `motocicletas`.`categoria_id`
+                                )
+                            )
+                        JOIN `users` ON
+                            (
+                                `users`.`id` = `motocicletas`.`user_id`
+                            )
+                        )
+                    JOIN `solicitud_servicios` ON
+                        (
+                            `solicitud_servicios`.`user_id` = `users`.`id`
+                        )
+                    )
+                JOIN `estado_solicitud_servicio` ON
+                    (
+                        `estado_solicitud_servicio`.`solicitud_id` = `solicitud_servicios`.`id`
+                    )
+                )
+            JOIN `detalle_solicituds` ON
+                (
+                    `detalle_solicituds`.`solicitud_servicio_id` = `solicitud_servicios`.`id`
+                )
+            )
+        JOIN `servicios` ON
+            (
+                `servicios`.`id` = `detalle_solicituds`.`servicio_id`
+            )
+        )
+    JOIN `detalle_solicitud_repuesto` ON
+        (
+            `detalle_solicitud_repuesto`.`detalle_solicitud_id` = `detalle_solicituds`.`id`
+        )
     )
         
         ");
